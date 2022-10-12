f3e356f5c6ac8c7831f7f93a9d7ce357fd12ee97
NiShiGara <91442220+NiShiGara@users.noreply.github.com>
2022-10-12 22:18:20 +1000

    Запросы начало

--- lab_2.py
+++ lab_2.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 con = sqlite3.connect("YSP.sqlite")
-
+"""
 con.executescript('''
 CREATE TABLE IF NOT EXISTS category (
  category_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -72,7 +72,7 @@ CREATE TABLE IF NOT EXISTS pattern_detail (
 
 CREATE TABLE IF NOT EXISTS favorite (
  favorite_id INTEGER PRIMARY KEY AUTOINCREMENT,
- user_id INTEGER,
+ users_id INTEGER,
  pattern_id INTEGER,
  FOREIGN KEY (users_id) REFERENCES users (users_id) ON DELETE CASCADE,
  FOREIGN KEY (pattern_id) REFERENCES pattern (pattern_id) ON DELETE CASCADE
@@ -101,8 +101,6 @@ VALUES
 ('Шорты'),
 ('Юбка');
 
-
-
 INSERT INTO users (users_login, users_password, OG, OT, OB, VB, CG, VG, SHG1, SHG2, H, SHS, OSH, DPL, OP, OZ, DR)
 VALUES
 ('NiShiGara','123456789qwerty',104, 84, 110, 21, 22, 31, 44, 34, 11, 38, 40, 12, 32, 17, 62),
@@ -113,7 +111,6 @@ VALUES
 ('IvanovIvan','Jdhe3421NBq',84, 64, 90, 19, 19.5, 26, 34, 29, 8.5, 33, 35, 11, 27, 14.5, 60),
 ('cur_tess','123zxcvbasQWE',100, 80, 106, 21, 21.5, 30, 42, 33, 10.5, 37, 39, 12, 31, 16.5, 62),
 ('yulexer','16q04w2001rty',96, 76, 102, 20, 21, 29, 40, 32, 10, 36, 38, 12, 30, 16, 62);
-
 
 INSERT INTO pattern (pattern_name, category_id)
 VALUES
@@ -190,19 +187,152 @@ VALUES
 (7,1),
 (7,4);
 
+
+
+INSERT INTO pattern_detail (pattern_id, detail_id)
+VALUES
+(1, 1), 
+(1, 2),
+(1, 3),
+(6, 4), 
+(6, 5), 
+(6, 6),
+(6, 7);
+
+INSERT INTO detail (detail_name)
+VALUES
+('Рукав футболки поло'),
+('Перед футболки поло'),
+('Спинка футболки поло'),
+('Одношовный рукав'),
+('Воротник-стойка'),
+('Манжеты рукавов'),
+('Перед рубашки'),
+('Спинка рубашки');
+
 INSERT INTO measure (measure_name)
 VALUES
-();
+('Обхват плеча'), 
+('Обхват талии'),
+('Обхват бедер'), 
+('Обхват шеи'), 
+('Длина рукава'),
+('Длина переда до талии'), 
+('Длина спины до талии'), 
+('Длина изделия по спинке'), 
+('Длина плеча'), 
+('Ширина проймы'), 
+('Ширина спины'), 
+('Ширина переда'), 
+('Высота плеча переда косая'),
+('Высота плеча косая'),
+('Глубина проймы'),
+('Обхват запястья'),
+('Длина горловины');
 
-INSERT INTO math_formula (formula, description)
+INSERT INTO detail_measure (detail_id, measure_id)
 VALUES
-();
-
+(1, 1),
+(1, 5), 
+(2, 4),
+(2, 9),
+(2, 13),
+(2, 10),
+(2, 12),
+(2, 6),
+(2, 2),
+(2, 3),
+(3, 4),
+(3, 9),
+(3, 14),
+(3, 10),
+(3, 11),
+(3, 2),
+(3, 3),
+(3, 15),
+(3, 7),
+(3, 8),
+(4, 5),
+(4, 1),
+(4, 15),
+(5, 17),
+(6, 16);
 ''')
+"""
+
 
 # сохраняем информацию в базе данных
 con.commit()
 cursor = con.cursor()
+
+############################################################################
+#                              ЗАДАНИЕ                                     #
+############################################################################
+
+# Вывести все избранные юбки пользователей. Сортировка сначала по логину, потом по названию изделия
+cursor.execute('''
+ SELECT
+ users_login,
+ pattern_name
+ FROM favorite
+ JOIN pattern USING (pattern_id)
+ JOIN users USING (users_id)
+ JOIN category USING (category_id)
+ WHERE category_name = :p_category
+ORDER BY users_login, pattern_name
+''',{"p_category": "Юбка"})
+print(cursor.fetchall())
+print()
+
+# Вывести выкройки, которые еще не добавили в избранное
+cursor.execute('''
+ SELECT
+ pattern_name
+ FROM pattern
+ LEFT JOIN favorite USING (pattern_id) 
+ WHERE favorite.pattern_id is NULL
+ORDER BY pattern_name
+''')
+print(cursor.fetchall())
+print()
+
+# вывести количество деталей у всех выкроек, сортировка по убыванию количества деталей
+cursor.execute('''
+SELECT pattern_name, COUNT(detail_id) as amount
+FROM pattern
+ LEFT JOIN pattern_detail USING (pattern_id)
+ LEFT JOIN detail USING (detail_id)
+GROUP BY pattern_name
+ORDER BY amount DESC
+''')
+print(cursor.fetchall())
+print()
+
+# выводит количество избранных выкроек пользователей, сортирует по убыванию количества, а потом по возрастанию логинов
+cursor.execute('''
+SELECT users_login, COUNT(favorite_id) as max
+FROM favorite
+ JOIN users USING (users_id)
+GROUP BY users_login
+ORDER BY max DESC, users_login
+''')
+print(cursor.fetchall())
+print()
+
+cursor.execute('''
+SELECT a.studentid, a.name, b.total_marks
+FROM student a, marks b
+WHERE a.studentid = b.studentid AND b.total_marks >
+(SELECT pattern_id
+FROM favorite
+WHERE MAX(pattern_id) 
+GROUP BY users_id
+ORDER BY 
+);
+''')
+print(cursor.fetchall())
+print()
+
 
 # cursor.execute("SELECT * FROM pattern")
 # print(cursor.fetchall())
